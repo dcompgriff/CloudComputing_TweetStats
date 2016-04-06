8b9c1e90b1c753165d8ee9af43b8ff9957bcd8df
Dan Griffin <dcompgriff@gmail.com>
2016-04-06 18:18:22 -0400

    Adde find ono map and reduce functions.

---
+++ problem5/findOnoMap.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys
+import string
+import json
+
+tweeterDict = {}
+
+for line in sys.stdin:
+    #Each tweet exists on a single line. Try to parse string to dict.
+    tweet = None
+    try:
+        tweet = json.loads(line)
+    except:
+        sys.stderr.write('Error parsing string to dict.')
+    #Get the user object.
+    userName = tweet['user']['screen_name']
+
+    if userName == 'PrezOno' or userName == 'prezono':
+        #Create new tweeter key for the user.
+        tweeterDict[userName.encode('ascii', 'ignore')] = 1
+
+# Print each tweeter's
+for key in tweeterDict.keys():
+    print '%s\t%s' % (key, tweeterDict[key])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
