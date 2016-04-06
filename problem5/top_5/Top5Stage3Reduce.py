c28c9d987ce2c0d6cea8127115140024187ba7c3
Dan Griffin <dcompgriff@gmail.com>
2016-04-06 14:49:40 -0400

    Implemented top 5 stage 3 map and reduce functions.

---
+++ problem5/top_5/Top5Stage3Reduce.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import sys
+import string
+import json
+
+top5 = [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
+
+for line in sys.stdin:
+    #Each tweet exists on a single line. Try to parse string to dict.
+    (key, val) = line.strip().split('\t', 1)
+
+    #Pull the tweeter name and average value from the string tuple.
+    tweeterName = None
+    tweeterAverage = 0
+    try:
+        tweeterName = val.strip().split(',')[0][2:-1]
+        tweeterAverage = val.strip().split(',')[1][1:-1]
+    except:
+        sys.stderr.write('Error parsing tuple in stage3 reduce.')
+
+    #Append the new tweeter to the list.
+    top5.append((tweeterName, float(tweeterAverage)))
+    #Sort the list.
+    top5 = sorted(top5, key=lambda item: item[1], reverse=True)
+    #Remove the smallest average tweeter from the list.
+    top5 = top5[:-1]
+
+#Print the final set of top 5.
+for tup in top5:
+    print '%s\t%s' % (tup[0], str(tup[1]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
