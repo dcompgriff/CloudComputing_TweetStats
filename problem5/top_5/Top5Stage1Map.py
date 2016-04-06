a4091519fa12062ec026a7ac3f569743f416f6be
Dan Griffin <dcompgriff@gmail.com>
2016-04-06 13:34:07 -0400

    Implemented top 5 stage 1 map and reduce functions.

--- problem5/top_5/Top5Stage1Map.py
+++ problem5/top_5/Top5Stage1Map.py
@@ -1,4 +1,56 @@
+#!/usr/bin/env python
 
-
-
+import sys
+import string
 import json
+
+tweeterDict = {}
+
+for line in sys.stdin:
+    #Each tweet exists on a single line. Try to parse string to dict.
+    tweet = None
+    try:
+        tweet = json.loads(line)
+    except:
+        sys.stderr.write('Error parsing string to dict.')
+    #Get the user object.
+    userName = tweet['user']['screen_name']
+    userTweet = tweet['text']
+
+    if userName in tweeterDict.keys():
+        #Increment tweeter tweet number.
+        tweeterDict[userName.encode('ascii', 'ignore') + '_CNT'] += 1
+        tweeterDict[userName.encode('ascii', 'ignore') + '_SUM'] += len(userTweet.encode('ascii', 'ignore'))
+    else:
+        #Create new tweeter key for the user.
+        tweeterDict[userName.encode('ascii', 'ignore') + '_CNT'] = 1
+        tweeterDict[userName.encode('ascii', 'ignore') + '_SUM'] = len(userTweet.encode('ascii', 'ignore'))
+
+# Print each tweeter's
+for key in tweeterDict.keys():
+    print '%s\t%s' % (key, tweeterDict[key])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- problem5/top_5/Top5Stage1Reduce.py
+++ problem5/top_5/Top5Stage1Reduce.py
@@ -1,2 +1,34 @@
+#!/usr/bin/env python
+import sys
 
+sum = 0
+currentTweeter = None
+
+for line in sys.stdin:
+    #Each key value pair from the map phase is output on a new
+    #line, with a tab delimiting the key and value. So, read the
+    #line, and parse our the key and value.
+    (key, val) = line.strip().split('\t', 1)
+
+    if currentTweeter != key:
+        if currentTweeter:
+            #If the currentTweeter isn't None, then new tweeter. Print out current tweeter values.
+            try:
+                print '%s\t%s' % (currentTweeter, sum)
+            except:
+                sys.stderr.write('Error printing tweet length averages in stage1 reduce.')
+            # _CNT should be encountered first, so set the total tweets to val.
+            sum = val
+        currentTweeter = key
+    else:
+        try:
+            sum += long(val)
+        except:
+            continue
+
+#Output the final tweeter and their number of tweets.
+try:
+    print '%s\t%s' % (currentTweeter, sum)
+except:
+    sys.stderr.write('Error printing final tweeter average in stage1 reduce.')
 
