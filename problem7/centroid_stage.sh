53be8c7747e745736845d0e2a18c1dafb310c3d6
Sam Wenke <samwenke@gmail.com>
2016-04-11 17:49:45 -0400

    [problem7] Add MR real version to shell command and update problem description :smirk:

--- problem7/centroid_stage.sh
+++ problem7/centroid_stage.sh
@@ -1,1 +1,1 @@
-cat ../input/part-03212 | ./LocationStage1Map.py | ./LocationStage1Reduce.py
+hadoop jar /root/hadoop-2.7.1/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar -input /data/twitter -output location_s1_out -file *.py -mapper LocationStage1Map.py -reducer LocationStage1Reduce.py
--- problem7/count_stage.sh
+++ problem7/count_stage.sh
@@ -1,1 +1,1 @@
-cat ../input/part-03212 | ./CountStage1Map.py | ./CountStage1Reduce.py
+hadoop jar /root/hadoop-2.7.1/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar -input /data/twitter -output location_ave_s1_out -file *.py -mapper CountStage1Map.py -reducer CountStage1Reduce.py
