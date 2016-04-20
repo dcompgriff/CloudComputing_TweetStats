245a0db297b4585360999e7846fbfe86cbe8db4e
Dan Griffin <unknown7979@gmail.com>
2016-04-20 09:02:11 -0400

    Added extra plots for ngram prob 1, and wrote the ngram prob 1 report.

--- spark_ngrams_problem1/plotProblem1.py
+++ spark_ngrams_problem1/plotProblem1.py
@@ -8,16 +8,23 @@ yearList = []
 countList = []
 
 
-with open('../output/ngrams_output/full_output', 'rb') as f:
+with open('../output/ngrams_output/full_output.txt', 'r') as f:
     for line in f.readlines():
         tupElems = line.split(',')
         yearList.append(tupElems[0][3:-1])
         countList.append(long(tupElems[1].strip()[:-2]))
 
 xVals = np.arange(0, len(yearList), 1)
-plt.bar(xVals, countList)
-plt.xticks(xVals, yearList, rotation='vertical')
-plt.savefig('./year_count_plot.png')
+xTicks = np.arange(0, len(yearList), 4)
+yearLabels = np.array(yearList)[xTicks]
+
+fig = plt.gcf()
+fig.set_size_inches(30, 10)
+#fig.savefig('test2png.png', dpi=100)
+plt.bar(xVals, countList, width=1)
+plt.xticks(xTicks, yearLabels, rotation='vertical')
+plt.show()
+#plt.savefig('./year_count_plot.png')
 
 
 
